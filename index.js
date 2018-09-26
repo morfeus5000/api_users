955215dd3131379ff5c16799af2f637bc0f37157
morfeus5000 <neocrow27@gmail.com>
2018-09-25 23:09:28 -0300

    ++++

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express()
 
 //Define request response in root URL (/)
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send({user:'palmeida',password: 'd23ds312a113wsdS'})
 })
 
 //Launch listening server on port 5800
